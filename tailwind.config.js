b63396f543a183fe3074256392359df1067744fe
baguskharisma288 <baguskharisma288@gmail.com>
2024-06-06 07:45:19 +0000

    about me and tech stack page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "about-me.html", "tech-stack.html"],
   theme: {
     extend: {
       colors: {
